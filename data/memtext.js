d3e1857e0fa54e64f6bb00149c0fb4fa36b32050
Rick Toews <rick.toews@gmail.com>
2017-03-13 07:29:31 -0700

    init memorize

---
+++ data/memtext.js
@@ -0,0 +1,25 @@
+var memtext = {
+    "passage": "Revelation 18",
+    "verses": [
+        {
+	    "verse": 1,
+	    "text": "After this, I saw another angel coming down from heaven. He had great authority, and the earth was illuminated by his splendor.",
+	    "wrong": []
+	},
+	{
+	    "verse": 2,
+	    "text": "With a mighty voice he shouted, \"Fallen! Fallen is Babylon the Great! She has become a home for demons and a haunt for every evil spirit, a haunt for every unclean and detestable bird.",
+	    "wrong": []
+	},
+	{
+	    "verse": 3,
+	    "text": "For all the nations have drunk the maddening wine of her adulteries. The kings of the earth committed adultery with her, and the merchants of the earth grew rich from her excessive luxuries.",
+	    "wrong": []
+	},
+	{
+	    "verse": 4,
+	    "text": "Then I heard another voice from heaven say, Come out of her, my people, so that you will not share in her sins, so that you will not receive any of her plagues.",
+	    "wrong": []
+	}
+    ]
+};
